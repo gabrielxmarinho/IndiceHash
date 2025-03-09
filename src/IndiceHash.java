b52868984980ba9c8ebf82397a44354978901c9c
Gabriel Xavier Santiago Marinho <gabrielxsmarinho@edu.unifor.br>
2025-03-09 15:45:16 -0300

    Ajustes

--- src/IndiceHash.java
+++ src/IndiceHash.java
@@ -10,7 +10,9 @@ public class IndiceHash {
 	
 	public int funcaoHash(String chaveBusca) {
 		//Pendente a definição da função 
-		return chaveBusca.length()%((int) numRegistros);
+		//Aponta para um bucket a partir do hash
+		//Garantir que fique entre 0 e qtdBuckets-1 [0,nBuckets-1]
+		return chaveBusca.length()%(this.buckets.length);
 	}
 	private long numRegistros;
 	
@@ -27,7 +29,7 @@ public class IndiceHash {
 	private int nBucketOverflow;
 	public IndiceHash(String nomeArquivo) {
 		this.nColisoes = 0;
-		nBucketOverflow = 0;
+		this.nBucketOverflow = 0;
 		try {
 			this.numRegistros = Files.lines(Paths.get(nomeArquivo)).count();
 		} catch (IOException e) {
@@ -38,9 +40,7 @@ public class IndiceHash {
 		try {
 			File arq = new File(nomeArquivo);
 			Scanner scanner= new Scanner(arq);
-			//Criando as Páginas
 			while(scanner.hasNextLine()) {
-				//Atingiu o número máximo de tuplas por página
 				String linha = scanner.nextLine();
 				int hash = this.funcaoHash(linha);
 				//Se o Bucket não foi criado ainda 
@@ -55,6 +55,7 @@ public class IndiceHash {
 					nBucketOverflow++;
 				}
 				else {
+					//O Bucket já existe
 					//Colisão
 					buckets[hash].get(buckets[hash].size()-1).getNumeroPagina().add(hash);
 					this.nColisoes++;
@@ -75,8 +76,9 @@ public class IndiceHash {
 				}
 			}
 			System.out.printf("Taxa de Colisões = %f%%\n",100 * (float) this.nColisoes/this.numRegistros);
-			System.out.printf("Taxa de Buckets Overflow = %f%%\n",100 * (float) this.nBucketOverflow/this.numRegistros);
-			//Inicializando parâmetros da Busca
+			//Taxacolisoes = ncolisoes/nRegistros
+			//TaxaBucketOverflow = nBucketOverflow/nBuckets
+			System.out.printf("Taxa de Buckets Overflow = %f%%\n",100 * (float) this.nBucketOverflow/this.buckets.length);
 			scanner.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -85,7 +87,7 @@ public class IndiceHash {
 	
 	//Busca pelo Índice
 	public int chaveBusca(String chave) throws Exception {
-		int qtd = 0;
+		int qtd = 0; //Quantidade de pesquisas
 		int hash = this.funcaoHash(chave);
 		for(int i=0;i<this.buckets[hash].size();i++) {
 			for(int j=0;j<this.buckets[hash].get(i).getNumeroPagina().size();j++) {
@@ -104,7 +106,7 @@ public class IndiceHash {
 	
 	//Full Table Scan
 	public int tableScan(String chave) throws Exception {
-		int qtd = 0;
+		int qtd = 0; //Quantidade de pesquisas
 		for(int i=0;i<this.tabela.getPaginas().length;i++) {
 			//Verificando se a página foi preenchida
 			if(this.tabela.getPaginas()[i]!=null) {
